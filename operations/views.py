c4d2038bea1a45d825fb2a020b93e5d3dbc6b54c
Michał Szleger <michal.szleger@gmail.com>
2024-12-25 13:15:28 +0100

    Add showing logged user only his own items

--- operations/views.py
+++ operations/views.py
@@ -9,8 +9,8 @@ class CategoryList(generics.ListCreateAPIView):
     '''
     A view for listing categories and adding new categories.
     '''
-    queryset = Category.objects.all()
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = None
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = CategorySerializer
 
     def perform_create(self, serializer):
@@ -19,22 +19,34 @@ class CategoryList(generics.ListCreateAPIView):
         '''
         serializer.save(owner=self.request.user)
 
+    def get_queryset(self):
+        '''
+        Override the queryset to return only categories created by the current user.
+        '''
+        return Category.objects.filter(owner=self.request.user)
+
 
 class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
     '''
     A view for retrieving, editing and deleting categories.
     '''
-    queryset = Category.objects.all()
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = None
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = CategorySerializer
+
+    def get_queryset(self):
+        '''
+        Override the queryset to return only categories created by the current user.
+        '''
+        return Category.objects.filter(owner=self.request.user)
 
 
 class CurrencyList(generics.ListCreateAPIView):
     '''
     A view for listing currencies and adding new currencies.
     '''
-    queryset = Currency.objects.all()
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = None
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = CurrencySerializer
 
     def perform_create(self, serializer):
@@ -43,22 +55,34 @@ class CurrencyList(generics.ListCreateAPIView):
         '''
         serializer.save(owner=self.request.user)
 
+    def get_queryset(self):
+        '''
+        Override the queryset to return only currencies created by the current user.
+        '''
+        return Currency.objects.filter(owner=self.request.user)
+
 
 class CurrencyDetail(generics.RetrieveUpdateDestroyAPIView):
     '''
     A view for retrieving, editing and deleting currencies.
     '''
-    queryset = Currency.objects.all()
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = None
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = CurrencySerializer
+
+    def get_queryset(self):
+        '''
+        Override the queryset to return only currencies created by the current user.
+        '''
+        return Currency.objects.filter(owner=self.request.user)
 
 
 class OperationList(generics.ListCreateAPIView):
     '''
     A view for listing operations and adding new operations.
     '''
-    queryset = Operation.objects.all()
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = None
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = OperationSerializer
 
     def perform_create(self, serializer):
@@ -67,14 +91,26 @@ class OperationList(generics.ListCreateAPIView):
         '''
         serializer.save(owner=self.request.user)
 
+    def get_queryset(self):
+        '''
+        Override the queryset to return only operations created by the current user.
+        '''
+        return Operation.objects.filter(owner=self.request.user)
+
 
 class OperationDetail(generics.RetrieveUpdateDestroyAPIView):
     '''
     A view for retrieving, editing and deleting operations.
     '''
-    queryset = Operation.objects.all()
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = None
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = OperationSerializer
+
+    def get_queryset(self):
+        '''
+        Override the queryset to return only operations created by the current user.
+        '''
+        return Operation.objects.filter(owner=self.request.user)
 
 
 class UserList(generics.ListAPIView):
