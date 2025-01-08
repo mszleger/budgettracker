38d142e309683c5c3486cbe9a4f8249ea960971a
Michał Szleger <michal.szleger@gmail.com>
2025-01-08 17:45:37 +0100

    Change prefix of api to api

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('api.urls')),
+    path('api', include('api.urls')),
     path('api-auth/', include('rest_framework.urls')),
 ]
